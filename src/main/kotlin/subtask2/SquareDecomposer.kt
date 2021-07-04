1fbfbc3464970e2ffe848007b27088678c594722
Bondidos <gglbond@gmail.com>
2021-07-04 21:33:15 +0300

    test 2

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,30 +1,70 @@
 package subtask2
 
 class SquareDecomposer {
-
+   // http://uneex.ru/FrBrGeorge/News/2017-02-01
     fun decomposeNumber(number: Int): Array<Int>? {
         val list = mutableListOf<ArrayList<Int>>()
         val remain = number.pow() - (number - 1).pow()
+       var count = 0f
+        var X = 0
+        var y = 0
+        var z = 0
+        var t = 0
 
-        for(x in 0 .. remain)
-            for(y in 0 .. remain)
-                for(z in 0 .. remain)
-                    for(t in 0 .. remain)
-                        if (x*x + y*y + z*z + t*t == remain){
-                            val collision = ArrayList<Int>()
-                            collision.add(x)
-                            collision.add(y)
-                            collision.add(z)
-                            collision.add(t)
-                            collision.add(number-1)
-                            if(collision.isIncreasing())
-                                list.add(collision)
-                        }
-       // list.forEach { println(it) }
+       for(x in 0 .. remain) {
+           for (y in x..remain) {
+               for (z in y..remain) {
+                   for (t in z..remain) {
+                       count++
+                       if (x * x + y * y + z * z + t * t == remain) {
+                           val collision = ArrayList<Int>()
+                           collision.add(x)
+                           collision.add(y)
+                           collision.add(z)
+                           collision.add(t)
+                           collision.add(number - 1)
+                           if (collision.isIncreasing())
+                               list.add(collision)
+                       }
+                   }
+                   }
+           }
+       }
+       N = 99
+       //scanf("%d", &N);
+       //scanf("%d", &N);
+       {
+           x = 0
+           while (x * x * 4 < N) {
+               x++
+           }
+       }
+       {
+           Y = x
+           while (x * x <= N) {
+               while (Y >= N && (Y - 1) * (Y - 1) * 3 >= N - x * x) Y--
+               y = Y.also({ Z = it })
+               while (y <= x && x * x + y * y <= N) {
+                   while (Z >= N && (Z - 1) * (Z - 1) * 2 >= N - x * x - y * y) Z--
+                   z = Z.also({ t = it })
+                   while (z <= y && x * x + y * y + z * z <= N) {
+                       while (t * t > N - x * x - y * y - z * z) t--
+                       count++
+                       if (x * x + y * y + z * z + t * t == N) println(x.toString() + "," + y + "," + z + "," + t + "," + N)
+                       z++
+                   }
+                   y++
+               }
+               x++
+           }
+       }
+       println(+count)
+       println(count)
+        list.forEach { println(it) }
        // val arr = list.findValid()
        // arr.forEach { println(it) }
-        //return list.findValid()
-        throw NotImplementedError("Not implemented")
+        return list.findValid()
+        //throw NotImplementedError("Not implemented")
     }
 }
 
