f6365b8f7eda7ada93642a11b876eceb9011555e
Bondidos <gglbond@gmail.com>
2021-07-05 14:15:20 +0300

    subtask 2 is done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,100 +1,27 @@
 package subtask2
 
 class SquareDecomposer {
-   // http://uneex.ru/FrBrGeorge/News/2017-02-01
+
     fun decomposeNumber(number: Int): Array<Int>? {
-        val list = mutableListOf<ArrayList<Int>>()
-        val remain = number.pow() - (number - 1).pow()
-       var count = 0f
-        var X = 0
-        var y = 0
-        var z = 0
-        var t = 0
-
-       for(x in 0 .. remain) {
-           for (y in x..remain) {
-               for (z in y..remain) {
-                   for (t in z..remain) {
-                       count++
-                       if (x * x + y * y + z * z + t * t == remain) {
-                           val collision = ArrayList<Int>()
-                           collision.add(x)
-                           collision.add(y)
-                           collision.add(z)
-                           collision.add(t)
-                           collision.add(number - 1)
-                           if (collision.isIncreasing())
-                               list.add(collision)
-                       }
-                   }
-                   }
-           }
-       }
-       N = 99
-       //scanf("%d", &N);
-       //scanf("%d", &N);
-       {
-           x = 0
-           while (x * x * 4 < N) {
-               x++
-           }
-       }
-       {
-           Y = x
-           while (x * x <= N) {
-               while (Y >= N && (Y - 1) * (Y - 1) * 3 >= N - x * x) Y--
-               y = Y.also({ Z = it })
-               while (y <= x && x * x + y * y <= N) {
-                   while (Z >= N && (Z - 1) * (Z - 1) * 2 >= N - x * x - y * y) Z--
-                   z = Z.also({ t = it })
-                   while (z <= y && x * x + y * y + z * z <= N) {
-                       while (t * t > N - x * x - y * y - z * z) t--
-                       count++
-                       if (x * x + y * y + z * z + t * t == N) println(x.toString() + "," + y + "," + z + "," + t + "," + N)
-                       z++
-                   }
-                   y++
-               }
-               x++
-           }
-       }
-       println(+count)
-       println(count)
-        list.forEach { println(it) }
-       // val arr = list.findValid()
-       // arr.forEach { println(it) }
-        return list.findValid()
-        //throw NotImplementedError("Not implemented")
+        return if (number < 3) return null
+        else calculate(number.pow(), number - 1)?.toTypedArray()
     }
 }
-
-    fun Int.pow() = this * this
-
-    fun ArrayList<Int>.isIncreasing(): Boolean{
-        for(i in this.size-1  downTo 1){
-            if(this[i] <= this[i-1])
-                return false
+fun calculate (number: Int, index: Int): ArrayList<Int>?{
+    for(i in index downTo 1){
+        val remain = number - i.pow()
+        if(remain == 0) return arrayListOf(i)
+        if(remain < 0) return null
+        var element = remain.sqrt()
+        if(element >= i) element = i-1
+        val result = calculate(remain, element)
+        if(result !=null){
+            result.add(i)
+            return result
         }
-        return true
     }
-fun MutableList<ArrayList<Int>>.findValid(): Array<Int>{
-    var buffer = ArrayList<Int>()
-    for (i in this){
-        if(this.indexOf(i) == 0)
-            buffer = i
-        if (buffer.size > i.size)
-            buffer = i
-        if (buffer.size == i.size)
-            buffer = compareArrays(buffer,i) ?:i
-    }
-    return buffer.toTypedArray()
+    return null
 }
-fun compareArrays(arr1:ArrayList<Int>, arr2:ArrayList<Int>): ArrayList<Int>{
-    for (i in arr1.size -1 downTo 1){
-        if(arr1 == arr2) return arr1
-        if(arr1[i] == arr2[i]) continue
-        if (arr1[i] < arr2[i]) return arr2
-        if (arr1[i] > arr2[i]) return arr1
-    }
-    return arr1
-}
+
+fun Int.pow() = this * this
+fun Int.sqrt() = kotlin.math.sqrt(this.toDouble()).toInt()
