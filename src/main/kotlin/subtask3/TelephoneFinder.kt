53a689d02c87a1e48206708a9f879143ccb8640e
Bondidos <gglbond@gmail.com>
2021-07-05 20:23:03 +0300

    commit: All done

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -3,31 +3,46 @@ package subtask3
 class TelephoneFinder {
 
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        val keyBoard = arrayOf<Array<String>>(
-            arrayOf("1","2","3"),
-            arrayOf("4","5","6"),
-            arrayOf("7","8","9"),
-            arrayOf("*","0","#")
+        val keyBoard = arrayOf(
+            arrayOf('1','2','3'),
+            arrayOf('4','5','6'),
+            arrayOf('7','8','9'),
+            arrayOf('*','0','#')
         )
-
-        val result = ArrayList<String>()
-        result.findNeighbors(number,keyBoard)
-        result.forEach {println(it)}
-        throw NotImplementedError("Not implemented")
+        return number.findNeighbors(keyBoard)
     }
 }
-fun ArrayList<String>.findNeighbors(number: String,keyBoard: Array<Array<String>>) {
-    var coordinates = 0 to 0
-    for(i in keyBoard.indices){
+fun String.findNeighbors(keyBoard: Array<Array<Char>>): Array<String>? {
+    val result = arrayListOf<String>()
+    var coordinates : Pair<Int,Int>?
+    for (i in this.indices) {
+        coordinates = findCoordinates(keyBoard, this[i])
+        if(coordinates == null) return null
+        if (coordinates.first - 1 >= 0) {
+            result.add(this.replaceRange(i..i, keyBoard[coordinates.first - 1][coordinates.second].toString()))
+        }
+        if (coordinates.first + 1 <= 3 &&
+            keyBoard[coordinates.first + 1][coordinates.second] != '*'  &&
+            keyBoard[coordinates.first + 1][coordinates.second] != '#') {
+            result.add(this.replaceRange(i .. i, keyBoard[coordinates.first + 1][coordinates.second].toString()))
+        }
+        if (coordinates.second - 1 >= 0 &&
+            keyBoard[coordinates.first][coordinates.second - 1] != '*') {
+            result.add(this.replaceRange(i..i, keyBoard[coordinates.first][coordinates.second - 1].toString()))
+        }
+        if (coordinates.second + 1 <= 2 &&
+            keyBoard[coordinates.first][coordinates.second + 1] != '#') {
+            result.add(this.replaceRange(i..i, keyBoard[coordinates.first][coordinates.second + 1].toString()))
+        }
+    }
+    return result.toTypedArray()
+}
+
+fun findCoordinates(keyBoard: Array<Array<Char>>, char: Char): Pair<Int,Int>?{
+
+    for(i in keyBoard.indices)
         for(j in keyBoard[i].indices)
-            if(number == keyBoard[i][j])
-                coordinates = i to j
-    }
-    for(i in 0 ..3)
-    when{
-        coordinates.first -1 >= 0 -> this.add(keyBoard[coordinates.first - 1][coordinates.second])
-        coordinates.first +1 <= 3 -> this.add(keyBoard[coordinates.first + 1][coordinates.second])
-        coordinates.second -1 >= 0 -> this.add(keyBoard[coordinates.first][coordinates.second - 1])
-        coordinates.second +1 >= 2 -> this.add(keyBoard[coordinates.first][coordinates.second + 1])
-    }
+            if(char == keyBoard[i][j])
+               return i to j
+    return  null
 }
